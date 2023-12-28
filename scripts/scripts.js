462c685a721eb4b89aa5aff382132e74ab90d30f
osvaldo <osvaldoalvesbrito789@gmail.com>
2023-12-28 19:48:58 -0300

    update

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,55 +1,75 @@
+let result = document.getElementById('result')
+let res = ''
 
-let memoria = ""
+document.getElementById('zero').addEventListener('click', function () {
+    res += '0'
+    result.innerText = res
+})
+document.getElementById('one').addEventListener('click', function () {
+    res += '1'
+    result.innerText = res
+})
+document.getElementById('two').addEventListener('click', function () {
+    res += '2'
+    result.innerText = res
+})
+document.getElementById('three').addEventListener('click', function () {
+    res += '3'
+    result.innerText = res
+})
+document.getElementById('four').addEventListener('click', function (ev) {
+    res += '4'
+    result.innerText = res
+})
+document.getElementById('five').addEventListener('click', function () {
+    res += '5'
+    result.innerText = res
+})
+document.getElementById('six').addEventListener('click', function () {
+    res += '6'
+    result.innerText = res
+})
+document.getElementById('seven').addEventListener('click', function () {
+    res += '7'
+    result.innerText = res
+})
+document.getElementById('eight').addEventListener('click', function () {
+    res += '8'
+    result.innerText = res
+})
+document.getElementById('none').addEventListener('click', function () {
+    res += '9'
+    result.innerText = res
+})
+document.getElementById('dot').addEventListener('click', function () {
+    res += '.'
+    result.innerText = res
+})
+document.getElementById('add').addEventListener('click', function () {
+    res += ' + '
+    result.innerText = res
+})
+document.getElementById('subtraction').addEventListener('click', function () {
+    res += ' - '
+    result.innerText = res
+})
+document.getElementById('multiplication').addEventListener('click', function () {
+    res += ' * '
+    result.innerText = res
+})
+document.getElementById('division').addEventListener('click', function () {
+    res += ' / '
+    result.innerText = res
+})
 
-function historico(hist){ // numeros e caracteres digitados
-  document.querySelector('div').textContent = hist
-}
+document.getElementById('equal').addEventListener('click', function () {
+    res = eval(res)
+    result.innerText = res
+})
 
-function mostrar(hist){ // numeros e caracteres digitados
-  let total = 0
-  let num = ""
-  for(let n of hist){
-    if(!isNaN(n)){
-      num += n
-    }
-    else if (n === '+'){
-      total += Number(num)
-    }
-  }
+document.getElementById('deleteEverything').addEventListener('click', function () {
+    res = ''
+    result.innerText = res
+})
 
 
-  document.querySelector("h3").textContent = total
-  total=0
-  num = ""
-}
-
-
-const addNumeros  = ()=>{ // add numeros
-
-  document.getElementById("um").addEventListener("click", ()=>{
-    historico(memoria += "1")
-  })
-  document.getElementById("dois").addEventListener("click", ()=>{
-    historico(memoria += "2")
-  })
-  document.getElementById("tres").addEventListener("click", ()=>{
-    historico(memoria += "3")
-  })
-  document.getElementById("quatro").addEventListener("click", ()=>{
-    historico(memoria += "4")
-  })
-
-  document.getElementById("soma").addEventListener("click", ()=>{
-    historico(memoria += "+")
-  })
-
-  document.getElementById("igual").addEventListener("click", ()=>{
-    mostrar(memoria)
-  })
-
-}
-
-
-
-
-addNumeros()
